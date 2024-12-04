dd71a6e913dbc36193b8ead60d83fe1c5b18ac84
Joseph Ligier <joseph.ligier@accenture.com>
2024-12-04 16:01:51 +0100

    Test for 7 regions

---
+++ eks-clustermesh-multi/remove-state.sh
@@ -0,0 +1,5 @@
+while pulumi stack --stack dev --show-urns | grep cilium | grep URN > /tmp/urn
+do
+        state=$(head -1 /tmp/urn | awk '{print $NF}')
+        echo "pulumi state delete --stack dev --target-dependents '$state'" | bash
+done
