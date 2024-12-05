1e6238172bfa87273eff2fdb927655e9ed0cb0fe
Joseph Ligier <joseph.ligier@accenture.com>
2024-12-05 21:51:49 +0100

    Fix remove-state.sh

--- eks-clustermesh-multi/remove-state.sh
+++ eks-clustermesh-multi/remove-state.sh
@@ -1,5 +1,5 @@
 while pulumi stack --show-urns | grep cilium | grep URN > /tmp/urn
 do
         state=$(head -1 /tmp/urn | awk '{print $NF}')
-        echo "pulumi state delete --stack dev --target-dependents '$state'" | bash
+        echo "pulumi state delete --target-dependents '$state'" | bash
 done
